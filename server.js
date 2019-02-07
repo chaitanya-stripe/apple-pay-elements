faeb3aff931a3f9e04abcd6a049d9711e0498d7f
Glitch (sally-cupcake-new-card) <none>
2019-02-07 03:24:03 +0000

    🐚🌠 Checkpoint ./server.js:2911894/363

--- server.js
+++ server.js
@@ -18,13 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-// app.get('/', function(request, response) {
-//   response.sendFile(__dirname + '/views/index.html');
-// });
 
 app.get("/", function(request, response) {
   response.render(__dirname + "/views/index.ejs");
@@ -59,27 +53,36 @@ app.get("/success", function(request, response) {
 app.post("/webhooks", function(request, response) {
   response.sendStatus(200);
   
-  console.log(request.body)
+  const intent = request.body.data.object
   
-  const intentId = request.body.id
-  const sourceId = request.body.data.object.source
+  // console.log(intent)
+
+  const intentId = intent.id
+  const intentStatus = intent.status
+  const sourceId = intent.source
   
-  stripe.sources.retrieve(sourceId, function(err, source) {
-    const customerEmailAddress = source.owner.email
-    
-    // Track your sale, create shipping labels, and do other business things here...
-    
-    console.log("Sending email confirmation to ", customerEmailAddress)
-    
-    const msg = {
-      to: customerEmailAddress,
-      from: 'test@example.com',
-      subject: 'Thanks for buying a MacGuffin',
-      text: `We hope you like your new MacGuffin. https://dashboard.stripe.com/test/payments/${intentId}`,
-      html: `<p>We hope you like your new MacGuffin. <a href="https://dashboard.stripe.com/test/payments/${intentId}">Dashboard</a></p>`,
-    };
-    sgMail.send(msg);
-  });
+  if (intentStatus === "succeeded") {
+    stripe.sources.retrieve(sourceId, function(err, source) {
+      const customerEmailAddress = source.owner.email
+
+      // Track your sale, create shipping labels, and do other business things here...
+
+      console.log("Sending email confirmation to ", customerEmailAddress)
+
+      const msg = {
+        to: customerEmailAddress,
+        from: 'test@example.com',
+        subject: 'Thanks for buying a MacGuffin',
+        text: `We hope you like your new MacGuffin. https://dashboard.stripe.com/test/payments/${intentId}`,
+        html: `<p>We hope you like your new MacGuffin. <a href="https://dashboard.stripe.com/test/payments/${intentId}">Dashboard</a></p>`,
+      };
+      sgMail.send(msg);
+    });
+  }
+  else {
+    // handle a failure
+    console.error("PaymentIntent did not succeed!", intentId)
+  }   
 })
 
 // listen for requests :)
