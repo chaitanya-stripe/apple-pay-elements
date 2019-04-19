962035fdc4deb59b4f0fb5c7d87fc9843d8e5a4d
Glitch (sally-cupcake-new-card) <none>
2019-04-19 12:34:33 +0000

    🚋🍢 Checkpoint ./server.js:2911894/248 ./views/payment.ejs:2911894/18

--- server.js
+++ server.js
@@ -20,11 +20,12 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get("/", function(request, response) {
-  response.render(__dirname + "/views/index.ejs");
-});
+// app.get("/", function(request, response) {
+//   response.render(__dirname + "/views/index.ejs");
+// });
 
-app.get("/payment", function(request, response) {
+// app.get("/payment", function(request, response) {
+app.get("/", function(request, response) {
   response.render(__dirname + "/views/payment.ejs", {
     STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
   });
@@ -38,16 +39,21 @@ app.get("/success", function(request, response) {
 })
 
 app.post('/process_payment', async (request, response) => {
+  console.log('about to call charges.create')
   const payment = await stripe.charges.create({
     source: request.body.token_id,
     amount: 1999,
     currency: 'eur',
   })
   
-  await response.send({
+  console.log('just called charges.create')
+  
+  response.send({
     payment_id: payment.id,
     payment_status: payment.status,
   })
+  
+  console.log('sent response to server')
 })
 
 // listen for requests
