1460e1be3c7d59fcfbf2661b1d075075274b0dc5
Glitch (sally-cupcake-new-card) <none>
2020-07-15 22:48:23 +0000

    👆⛴ Checkpoint ./server.js:19093412/22

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/process_payment', async (request, response) => {
           request_three_d_secure: 'any',
         },
       },
-      amount: 199,
+      amount: 166,
       currency: 'usd',
       confirmation_method: 'manual',
       confirm: true,
