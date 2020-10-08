7fde27a61c1980025ca8ca9e8060193c1c66b7b4
Glitch (sally-cupcake-new-card) <none>
2020-10-08 20:52:48 +0000

    🌄🏂 Checkpoint ./server.js:19093412/48

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/success", function(request, response) {
 app.post('/process_payment', async (request, response) => {
   try {
     const payment = await stripe.paymentIntents.create({
-      // customer: 'cu_1Ha4FLLGHh7VlXNwiTWAfpRh',
+      customer: 'cus_IAP3iJrgjgEphv',
       payment_method: request.body.payment_method_id,
       payment_method_options: {
         card: {
